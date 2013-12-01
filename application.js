eb98edfef57680631700aa52975b6cf2838c01b6
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2013-11-30 19:50:21 -0500

    Cannot support multiple audio inputs yet.
    WebRTC doesn't support multiple audio streams, it seems. Stopping for now.

--- application.js
+++ application.js
@@ -1,8 +1,13 @@
-var Classroom = (function($) {
-
+var Classroom = (function($, window) {
+  if (!$) { throw "jQuery required."; }
   // Instance stores the reference to the Singleton.
   var instance;
+  var self = this;
 
+  var AudioContext = window.AudioContext ||
+                     window.webkitAudioContext;
+
+  //---------------------------------------------------------------------------
   // validate WebRTC support.
   function initializeWebRTC() {
     if (!webrtcDetectedBrowser) {
@@ -16,7 +21,28 @@ var Classroom = (function($) {
     return true;
   }
 
+  //---------------------------------------------------------------------------
+  // initialize WebAudio.
+  function initializeWebAudio() {
+    if (!AudioContext) {
+      console.log("Browser does not support WebAudio.");
+      return false;
+    }
+    return true;
+  }
 
+  //---------------------------------------------------------------------------
+  // setup WebAudio.
+  function setupWebAudio() {
+    self.webAudio = {};
+    self.webAudio.context = new AudioContext();
+    self.webAudio.inputs = [];
+    self.webAudio.compressor =
+      self.webAudio.context.createDynamicsCompressor();
+    //self.webAudio.compressor.connect(self.webAudio.context.destination);
+  }
+
+  //---------------------------------------------------------------------------
   // returns a map of audio sources key == source-id, value == label/name
   function _getAudioSources(callback) {
     MediaStreamTrack.getSources(function(sourceInfos) {
@@ -32,23 +58,55 @@ var Classroom = (function($) {
     });
   }
 
+  //---------------------------------------------------------------------------
+  // add an input source.
+  function _addAudioInput(sourceId, successcallback, errorcallback) {
+    var constraints = {
+      audio: { optional: [ {sourceId: sourceId} ] }
+    };
+    getUserMedia(constraints, function(stream) {
+      var inputStream = self.webAudio.context.createMediaStreamSource(stream);
+      var gainNode = self.webAudio.context.createGain();
+      gainNode.gain.value = 0.1;
+
+      inputStream.connect(gainNode);
+      gainNode.connect(self.webAudio.context.destination);
+
+      self.webAudio.inputs.push({
+        sourceId : sourceId,
+        webRTCStream : stream,
+        mediaStream : inputStream,
+        gainNode: gainNode
+      });
+      if (successcallback) {
+        successcallback(self.webAudio.inputs[self.webAudio.inputs.length-1]);
+      }
+    }, function(error) {
+      if (errorcallback) { errorcallback(error); }
+      else { console.log("Classroom Error: " + error); }
+    });
+  }
+
+  //---------------------------------------------------------------------------
   function constructor() {
-    if (!initializeWebRTC()) { return null; }
+    if (!initializeWebRTC() || !initializeWebAudio()) { return null; }
 
     return {
-      getAudioSources : _getAudioSources
+      getAudioSources : _getAudioSources,
+      addAudioInput : _addAudioInput
     };
   }
 
-
+  //---------------------------------------------------------------------------
   return {
     // Get access to the singleton instance.
     getInstance: function() {
       if (!instance) {
         instance = constructor();
+        setupWebAudio();
       }
       return instance;
     }
   };
 
-})($);
+})($, window);
