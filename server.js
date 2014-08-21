c95dfd0caa34dc89418f5bf647da22b54c0f4c4f
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2014-08-20 21:25:27 -0400

    Add PeerJS

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 var static = require('node-static');
 
+/* setup static page server */
 var port = process.env.OPENSHIFT_NODEJS_PORT;
 var host = process.env.OPENSHIFT_NODEJS_IP;
 
 var fileServer = new static.Server("./public");
-
 var http = require('http');
 http.createServer(function (request, response) {
   request.addListener('end', function () {
     fileServer.serve(request, response);
   }).resume();
 }).listen(port, host, function() {
-
+  console.log("Static page server started on " + port);
 });
+
+
+/* setup PeerJS server */
+//var PeerServer = require('peer').PeerServer;
+//var server = new PeerServer({port:9000, path: '/mclassroom'});
